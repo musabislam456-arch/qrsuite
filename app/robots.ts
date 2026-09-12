38c3361379c01c9959595b197568c14e3e958cc4
Musab Bin Umair <musabislam456@gmail.com>
2026-09-12 16:54:59 +0500

    fix: correct production domain (qrsuite.toolbay.site)

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://qrsuite.dev/sitemap.xml',
+    sitemap: 'https://qrsuite.toolbay.site/sitemap.xml',
   };
 }
